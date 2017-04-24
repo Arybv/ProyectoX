65dfafcceb1d43280c6d8356c2456b83209f53a5
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-04-24 18:01:31 -0500

    iconos header

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,25 @@
-window.addEventListener('load', function(){
-
+window.addEventListener('load', addIcons);
+  //function addPrincipal(){}
   var icons = document.getElementsByClassName("icon-redes");
   for (var i = 0; i < icons.length; i++) {
     addIcons(icons[i]);
-  }
+
 
   function addIcons(icons){
     var ulIco = document.createElement("ul");
     icons.appendChild(ulIco);
 
-    addClass("icon-facebook");
-    addClass("icon-twitter");
-    addClass("icon-share");
+    var clasesIcon = ["icon-facebook", "icon-share"];
+    for (var j = 0; j < clasesIcon.length; j++) {
 
-    function addClass(clase){
-      var liIco = document.createElement("li");
-      ulIco.appendChild(liIco);
-      var span = document.createElement("span");
-      span.setAttribute("class", clase);
-      liIco.appendChild(span);
-    }
+    function addClass(array){
+    var liIco = document.createElement("li");
+    ulIco.appendChild(liIco);
+
+    var span = document.createElement("span");
+    span.setAttribute("class", array[j]);
+    liIco.appendChild(span);
+    } addClass(clasesIcon);
   }
-
-})
+}
+}
